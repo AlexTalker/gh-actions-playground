2846b51fe6e2d71eaff04864007306ba9e8a0fbf
Alex Talker <3531127+AlexTalker@users.noreply.github.com>
2024-10-19 23:01:29 +0000

    Playing with libvirt + vagrant #10

--- libvirt/setup.sh
+++ libvirt/setup.sh
@@ -7,6 +7,8 @@ echo "ROOTDIR=${SCRIPTDIR}" > "${SCRIPTDIR}"/.env
 mkdir "${SCRIPTDIR}"/vagrant.d
 mkdir "${SCRIPTDIR}"/workdir
 
+cp Vagrantfile "${SCRIPTDIR}"/workdir/
+
 cd "${SCRIPTDIR}"
 
 exec podman-compose build

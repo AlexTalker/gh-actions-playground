72d306e651c837962adfde4e79deb2da8901b6db
Alex Talker <3531127+AlexTalker@users.noreply.github.com>
2024-10-20 00:56:40 +0000

    Playing with libvirt + vagrant #23

--- libvirt/run.sh
+++ libvirt/run.sh
@@ -4,4 +4,4 @@ SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd "${SCRIPTDIR}"
 
-exec podman-compose run --rm --entrypoint /bin/bash vagrant "$@"
+exec podman-compose run --rm vagrant "$@"

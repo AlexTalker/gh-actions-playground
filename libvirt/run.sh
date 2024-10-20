3e3caeb7d8abbda851e8d10edc5af8245115deee
Alex Talker <3531127+AlexTalker@users.noreply.github.com>
2024-10-20 02:30:27 +0000

    Playing with libvirt + vagrant #29

--- libvirt/run.sh
+++ libvirt/run.sh
@@ -4,4 +4,7 @@ SCRIPTDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 cd "${SCRIPTDIR}"
 
-exec podman-compose run --rm vagrant "$@"
+# Steal original UID/GID
+export PODMAN_USERNS=keep-id
+
+exec podman-compose run --rm --entrypoint /bin/bash vagrant "$@"

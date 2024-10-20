c688f60822ecb1ab30e8785151a460367e3a3c6f
Alex Talker <3531127+AlexTalker@users.noreply.github.com>
2024-10-20 12:05:41 +0000

    Playing with libvirt + vagrant #36

---
+++ libvirt/sudo.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+USER_NAME="$(id -un)"
+PRIMARY_GROUP_NAME="$(id -gn)"
+# Default for Ubuntu
+LIBVIRT_GROUP="${LIBVIRT_GROUP:-libvirt}"
+
+# newgrp and sg require password
+# but since we're already blessed with sudo
+# we can promote the added group as primary
+# and then return back the original one
+
+# This can take 12 args from argc maximum, not that anyone will hit it =)
+
+exec sudo --preserve-env --group "${LIBVIRT_GROUP}" --user "${USER_NAME}" \
+     sudo --preserve-env --group "${PRIMARY_GROUP_NAME}" --user "${USER_NAME} \
+     "$@"
